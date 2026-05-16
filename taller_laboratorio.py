db714ef996cae5258340ffc5a28728ba14ea9eb6
LEIDY2695 <leidyj2695@gmail.com>
2026-05-16 16:22:33 -0500

    Cambio de variables y de estructura de los ciclos

--- taller_laboratorio.py
+++ taller_laboratorio.py
@@ -1,35 +1,31 @@
 # Varibales
 
-galones = 0.0
-total_kilometros = 0.0
-octanaje = 0
-conteo_tanqueo = 0
-conteo_octanaje = 0
+total_km = 0
+total_galones = 0
+mejor_rendimiento = 0
+total_tanqueos = 0
+tanqueos_extras = 0
 
-programa_ejecutandose = True
-primera_entrada = True
+# Primer ingreso de galones
+galones = float(input())
 
-while programa_ejecutandose:
-    galones_cargados_str = input()
-    galones_cargados = float(galones_cargados_str)
-
-    if primera_entrada and galones_cargados == 0:
-        print("No data provided.")
-        programa_ejecutandose = False
-    elif galones_cargados == 0:
-        programa_ejecutandose = False
-    else:
-        kilometros_recorridos_str = input()
-        kilometros_recorridos = float(kilometros_recorridos_str)
-
-        octanaje_valido = False    #controlar variable del octanaje
-        octanaje = 0
-    while not octanaje_valido:
-        octanaje_str = input()   #valor sin texto
-        octanaje = int(octanaje_str)   #valor entero
-
-        if  81<= octanaje <=98:  #condicion que debe cumplir 
-            octanaje_valido = True
-        else:
+# Caso especial
+if galones == 0:
+    print("No data provided.")
+else:
+    #Ciclo principal
+    while galones != 0:
+        # Kilometros recorridos
+        km = float(input())
+        # Octanaje
+        octanaje = int(input())
+        # Validacion del octanaje
+        while octanaje < 81 or octanaje > 98:
             print("OCTANAJE INVALIDO")
-        
+            octanaje = int(input())
+        # Calcular rendimiento 
+        rendimiento = km / galones
+        # Acumular datos
+        total_km += km
+        total_galones += galones
+        total_tanqueos += 1
